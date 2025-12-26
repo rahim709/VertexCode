ead4d84d53a9d1082dc8a0b21e0694c148ff5ca5
rahim709 <abdurrahimer2004@gmail.com>
2025-12-26 17:08:30 +0530

    fix baseURL

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: import.meta.env.BACKEND_URL, //means our backend is hosted in this url
+    baseURL: "https://vertex-code-pi.vercel.app/",
+    //means our backend is hosted in this url
     withCredentials: true,      // means browser ko bata raha hu iske saath cookies ko attach kr dena
     headers: {
         'Content-Type': 'application/json'  // jo bhi data vej raha hu oo json format me h
