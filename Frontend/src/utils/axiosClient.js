5a8ea712e98eaec2c2eebc5b2ee47ee58afaf07a
rahim709 <abdurrahimer2004@gmail.com>
2025-12-26 11:56:51 +0530

    axiosClient.js

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient =  axios.create({
-    baseURL: import.meta.env.VITE_API_URL, //means our backend is hosted in this url
+    baseURL: import.meta.env.BACKEND_URL, //means our backend is hosted in this url
     withCredentials: true,      // means browser ko bata raha hu iske saath cookies ko attach kr dena
     headers: {
         'Content-Type': 'application/json'  // jo bhi data vej raha hu oo json format me h
